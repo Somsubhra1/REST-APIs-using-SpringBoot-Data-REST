ea0b739da67db5e067a87502d5b7ec19ab5c45f7
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-11 12:03:03 +0530

    added dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Alien values (101,'Navin','Java');
+insert into Alien values (102,'Kiran','Android');
+insert into Alien values (103,'Pravin','Python');
+insert into Alien values (104,'Komal','Java');
+insert into Alien values (105,'Rafiq','Android');
